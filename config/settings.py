5f52432c90874fe0e7772e202138409548b64d39
koki-sasaki <haiku130819@gmail.com>
2022-09-27 11:55:48 +0900

    staticをsettings.pyに追加

--- config/settings.py
+++ config/settings.py
@@ -118,7 +118,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+import os
+STATICFILED_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 STATIC_URL = '/static/'
 
 # Default primary key field type
@@ -130,7 +133,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'hamburger.CustomUser'
 
 #mediaのルート
-import os
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
